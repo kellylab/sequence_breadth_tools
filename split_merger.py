b7d9b2aa7569864943955b95f5f1a9c1421a97cc
Johnny Connolly <58537817+Elcquid@users.noreply.github.com>
2024-09-19 11:22:47 -0400

    Cleaning up split_merger.py

--- split_merger.py
+++ split_merger.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[21]:
-
-
 import pandas as pd
 import os
 import numpy as np
@@ -11,53 +5,22 @@ import sqlite3
 import csv
 
 
-# In[22]:
-
-
-#os.chdir('Downloads')
-
-
-# In[ ]:
-
-
-
-
-
-# In[23]:
-
-
-
-
 def export_table_to_csv(db_file, table_name, csv_file):
-    # Connect to the SQLite database
+    #Find and extract table from SQLdb
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
-
-    # Execute SQL query to select all data from the table
     query = f"SELECT * FROM {table_name}"
     cursor.execute(query)
-
-    # Fetch all rows from the table
     rows = cursor.fetchall()
-
-    # Get column names
     column_names = [description[0] for description in cursor.description]
-
-    # Write the data to a CSV file
+    #Write to CSV
     with open(csv_file, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
-        
-        # Write the column headers
         csv_writer.writerow(column_names)
-        
-        # Write the data rows
         csv_writer.writerows(rows)
 
     # Close the database connection
     conn.close()
-
-
-# In[4]:
 
 
 def fix_split(defline):
@@ -67,18 +30,14 @@ def fix_contig(defline):
     return(defline.split('_genomic')[0])
 
 
-# In[25]:
 
 
 def detection_pop_csv():
-#Export detection data from SQL to csv. Read into pandas, remove numbers from splits
+#Export detection data from SQL to CSV. Read into pandas, remove numbers from splits
     db_file = 'PROFILE.db'
     table_name = 'detection_splits'
     csv_file = 'detection_splits.csv'
     export_table_to_csv(db_file, table_name, csv_file)
-
-
-# In[26]:
 
 
 def main():
@@ -111,46 +70,11 @@ def main():
         os.chdir('..')
 
 
-# In[8]:
 
 
 if __name__ == '__main__':
     main()
 
-
-# In[9]:
-
-
-#mean_df.to_csv('mean_detections.txt',sep='\t')
-#db_file = 'PROFILE.db'
-#table_name = 'mean_coverage_splits'
-#csv_file = 'mean_coverage_splits.csv'
-#export_table_to_csv(db_file, table_name, csv_file)
-#dataset = pd.read_csv('mean_coverage_splits.csv')
-#dataset['item'] = dataset['item'].apply(fix_split)
-#mean_df = dataset.groupby(['layer', 'item'])['value'].mean().unstack()
-#mean_df.to_csv('mean_coverages.txt',sep='\t')
-
-
-# In[ ]:
-
-
-
-
-
-# In[12]:
-
-
-#os.chdir('..')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
